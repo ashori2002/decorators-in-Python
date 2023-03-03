3acb43309e1fae10ec22de274c03ab9d3de18da1
ashori2002 <mohammadashori2002@gmail.com>
2023-03-04 01:06:47 +0330

    decorators step 1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 
+def my_decorator(func):
+    def wrapper():
+        print("Line number is 1")
+        func()
+        print("Line number is 3")
+    return wrapper()
+def test():
+    print("Line number is 2")
+
+my_decorator(test)
